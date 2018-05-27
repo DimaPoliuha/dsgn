f18fe8bde4a712dda0f0494f3da255cb23182a9e
Dmitry <dmitry.polyuha@gmail.com>
2018-05-27 19:01:00 +0300

    update models

---
+++ application/models/Account.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Dmitry
+ * Date: 27.05.2018
+ * Time: 19:00
+ */
+
+namespace application\models;
+
+use application\core\Model;
+
+class Account extends Model {
+
+}
--- application/models/Main.php
+++ application/models/Main.php
@@ -12,8 +12,39 @@ use application\core\Model;
 
 class Main extends Model {
 
+    public function getProjects(){
+        $path = 'application/views/main/projects.php';
+        if(file_exists($path)){
+            $result = require_once $path;
+            return $result;
+        }
+        return false;
+    }
+
+    public function getStudio(){
+        $path = 'application/views/main/studio.php';
+        if(file_exists($path)){
+            $result = require_once $path;
+            return $result;
+        }
+        return false;
+    }
+
     public function getNews(){
-        $result = $this->db->select('title', 'description')->from('news')->execute();
-        return $result;
+        $path = 'application/views/main/news.php';
+        if(file_exists($path)){
+            $result = require_once $path;
+            return $result;
+        }
+        return false;
+    }
+
+    public function getContact(){
+        $path = 'application/views/main/contact.php';
+        if(file_exists($path)){
+            $result = require_once $path;
+            return $result;
+        }
+        return false;
     }
 }
