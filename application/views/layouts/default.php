0207b28bf4a9c8f7645f2195802d01df51c430b8
Dmitry <dmitry.polyuha@gmail.com>
2018-05-27 17:11:22 +0300

    update views

--- application/views/layouts/default.php
+++ application/views/layouts/default.php
@@ -12,17 +12,16 @@
 <head>
     <title>DSGN — <?php echo $title ?></title>
     <meta charset="utf-8"/>
-    <link rel="stylesheet" href="assets/main.css" />
-    <script src="https://code.jquery.com/jquery-3.3.1.min.js" async></script>
-    <script src="https://unpkg.com/isotope-layout@3.0.6/dist/isotope.pkgd.min.js" async></script>
-    <script src="assets/slideout.js" async></script>
-    <script src="assets/script.js" async></script>
+    <link rel="stylesheet" href="/<?php echo \application\core\ROOT_URL?>public/styles/main.css" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="icon" type="image/png" href="images/logo.png"/>
+    <link rel="icon" type="image/png" href="/<?php echo \application\core\ROOT_URL?>public/images/logo.png"/>
     <script type="text/javascript" src="/<?php echo \application\core\ROOT_URL?>public/scripts/jquery-3.3.1.min.js"></script>
+    <script type="text/javascript" src="/<?php echo \application\core\ROOT_URL?>public/scripts/masonry.pkgd.min.js"></script>
+    <script type="text/javascript" src="/<?php echo \application\core\ROOT_URL?>public/scripts/script.js"></script>
     <script type="text/javascript" src="/<?php echo \application\core\ROOT_URL?>public/scripts/form.js"></script>
 </head>
 <body>
+
 <?php
     echo $content;
 ?>
--- application/views/main/index.php
+++ application/views/main/index.php
@@ -7,10 +7,33 @@
  */
 ?>
 
-<p>main page</p>
-
-<?php //foreach ($news as $new): ?>
-<!--    <h3>--><?php //echo $new['title'] ?><!--</h3>-->
-<!--    <p>--><?php //echo $new['description'] ?><!--</p>-->
-<!--    <hr>-->
-<?php //endforeach;?>
+<script type="text/javascript" src="/<?php echo \application\core\ROOT_URL?>public/scripts/script.js"></script>
+<!--header-->
+<div class="grid">
+    <div class="grid-sizer"></div>
+    <div id="head-main" class="grid-item grid-item--width4 grid-item--height3">
+        <img id="logo" src="/<?php echo \application\core\ROOT_URL?>public/images/logo.png">
+        <h2>ASSOCIATES<br>STUDIO<br>DESIGN</h2>
+    </div>
+    <div class="grid-item grid-item--width2 grid-item--height3">2</div>
+</div>
+<div class="grid">
+    <div class="grid-sizer"></div>
+    <div class="grid-item grid-item--width2 grid-item--height3">3</div>
+    <div class="grid-item grid-item--width2 grid-item--height3">4</div>
+    <div class="grid-item grid-item--width2">5</div>
+    <div class="grid-item grid-item--width2 grid-item--height3">6</div>
+    <div class="grid-item grid-item--height3">7</div>
+    <div class="grid-item grid-item--width3 grid-item--height3">8</div>
+    <div class="grid-item grid-item--width2">9</div>
+    <div class="grid-item grid-item--width3 grid-item--height2">10</div>
+    <div class="grid-item grid-item--height2">11</div>
+    <div class="grid-item grid-item--width2 grid-item--height2">12</div>
+    <div class="grid-item grid-item--width2 grid-item--height3">13</div>
+    <div class="grid-item grid-item--width4 grid-item--height3">14</div>
+    <div class="grid-item grid-item--width6 grid-item--height2">15</div>
+    <div class="grid-item grid-item--width2 grid-item--height3">16</div>
+    <div class="grid-item grid-item--width4 grid-item--height3">17</div>
+    <div class="grid-item grid-item--width3 grid-item--height4">18</div>
+    <div class="grid-item grid-item--width3 grid-item--height4">19</div>
+</div>
